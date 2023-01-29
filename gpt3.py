ae68514380ab5cd8426ed5bacc4c3270f0d376f0
Pico2x <pico2x@gmail.com>
2023-01-29 17:28:13 -0300

    Adds a basic integration with gpt3

---
+++ gpt3.py
@@ -0,0 +1,21 @@
+import openai
+
+class GPT3API:
+    def __init__(self, token):
+        self.token = token
+        openai.api_key = token
+
+    def generate_response(self, prompt):
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            best_of=1
+        )
+
+        return response.choices[0].text
+
