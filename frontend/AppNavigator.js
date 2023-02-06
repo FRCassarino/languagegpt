fa5b3b31051176601248baf9231a4e02e68f4743
Pico2x <pico2x@gmail.com>
2023-02-06 16:52:12 -0300

    Show config button, hide back arrow

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import LanguageSelectionScreen from './LanguageSelectionScreen';
 import ChatScreen from './ChatScreen';
-
+import { TouchableOpacity, View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 const Stack = createStackNavigator();
 
 const AppNavigator = () => (
@@ -14,13 +15,22 @@ const AppNavigator = () => (
       options={{ title: 'Language Selection' }}
     />
     <Stack.Screen
-      name="Chat"
-      component={ChatScreen}
-      options={({ route }) => ({ title: route.params.language.charAt(0).toUpperCase() + route.params.language.slice(1) })}
-    />
+  name="Chat"
+  component={ChatScreen}
+  options={({ navigation, route }) => ({
+    title: route.params.language.charAt(0).toUpperCase() + route.params.language.slice(1),
+    headerRight: () => (
+     <TouchableOpacity onPress={() => navigation.navigate('LanguageSelection')} style={{marginRight: 10}}> 
+         <Ionicons name="settings" size={22} color="gray"/>
+     </TouchableOpacity>
+    ),
+    headerLeft: () => (<View />),
+  })}
+/>
   </Stack.Navigator>
 );
 
 export default AppNavigator;
+
 
 
