30e228856f589e9b47eff61d1cccdca212170159
Pico2x <pico2x@gmail.com>
2023-02-16 17:39:39 -0300

    Some UX changes
    - Added Warning sign to Notes.
    - Made fonts bigger.
    - Translate is now an icon under reply.

--- frontend/ChatList.js
+++ frontend/ChatList.js
@@ -1,38 +1,58 @@
 import React from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
 
 const styles = StyleSheet.create({
-    noteContainer: {
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: 0,
-        marginBottom: 10,
-      },
-      chatBubble: {
-        padding: 10,
-        borderRadius: 10,
-        maxWidth: "70%",
-      },
-      userChatBubble: {
-        backgroundColor: "#F0FFFF",
-        alignSelf: "flex-end",
-      },
-      chatbotChatBubble: {
-        backgroundColor: "#F5F5DC",
-        alignSelf: "flex-start",
-      },
+  noteContainer: {
+    flexDirection: "col",
+    alignItems: "center",
+    marginTop: 0,
+    marginBottom: 10,
+  },
+  warningIcon: {
+    marginRight: 5,
+  },
+  chatBubble: {
+    padding: 10,
+    borderRadius: 10,
+    maxWidth: '70%',
+  },
+  userChatBubble: {
+    backgroundColor: "#F0FFFF",
+    alignSelf: "flex-end",
+  },
+  chatbotChatBubble: {
+    backgroundColor: "#F5F5DC",
+    alignSelf: "flex-start",
+  },
+  translateIcon: {
+    color: "gray",
+    paddingVertical: 5,
+  },
+  listenIcon: {
+    color: "gray",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
 });
 
-
-const ChatList = ({ conversation, toggleTranslation, showTranslation, setModalMessage, setModalIndex, setModalVisible, loading}) => {
+const ChatList = ({
+  conversation,
+  toggleTranslation,
+  showTranslation,
+  setModalMessage,
+  setModalIndex,
+  setModalVisible,
+  loading,
+}) => {
   return (
     <View style={{ padding: 10 }}>
       {conversation.map((item, index) => (
         <View key={index}>
           {item.chatbot && item.chatbot.note !== "No errors." && (
             <View style={styles.noteContainer}>
-              <Text style={{ fontSize: 12, color: "gray" }}>
+              <FontAwesome name="warning" size={16} color="indianred" style={styles.warningIcon} />
+              <Text style={{ fontSize: 16, color: "gray" }}>
                 {" "}
                 {item.chatbot.note}
               </Text>
@@ -48,23 +68,20 @@ const ChatList = ({ conversation, toggleTranslation, showTranslation, setModalMe
                 setModalIndex(index);
               }
             }}
-            onPress={() => toggleTranslation(index)}
           >
             <View
               style={[
                 styles.chatBubble,
-                item.user
-                  ? styles.userChatBubble
-                  : styles.chatbotChatBubble,
+                item.user ? styles.userChatBubble : styles.chatbotChatBubble,
               ]}
             >
-              <Text style={{ fontSize: 16 }}>
+              <Text style={{ fontSize: 18 }}>
                 {item.chatbot ? item.chatbot.message : item.user}
               </Text>
               {item.chatbot && showTranslation === index && (
                 <Text
                   style={{
-                    fontSize: 12,
+                    fontSize: 14,
                     fontStyle: "italic",
                     color: "gray",
                   }}
@@ -72,6 +89,9 @@ const ChatList = ({ conversation, toggleTranslation, showTranslation, setModalMe
                   {"(" + item.chatbot.translation + ")"}
                 </Text>
               )}
+              <TouchableOpacity onPress={() => toggleTranslation(index)}>
+                <FontAwesome name="language" size={20} color="gray" style={styles.translateIcon} />
+              </TouchableOpacity>
             </View>
           </TouchableOpacity>
         </View>
